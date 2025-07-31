fb645e29876e2dd4948e85c28baa83b0a421e9b3
Maria Eduarda <ma.eferreira20@gmail.com>
2025-07-31 19:46:55 -0300

    nova fuction de card
    mostrando novas opções de adicionar card

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@
 import './App.css'
 import Card from './Card';
 function App() {
-const item1 ={
-  nome: 'Rick Sanchez',
-  foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-}
+  const item1 = {
+    nome: 'Rick Sanchez',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
 
-const item2 ={
-  nome: 'morty Smith',
-  foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-}
+  const item2 = {
+    nome: 'morty Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
 
-const item3 ={
-  nome: 'Summer Smith',
-  foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-}
+  const item3 = {
+    nome: 'Summer Smith',
+    foto: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  const personagens = [item1, item2, item3]
 
   return (
     <>
-      <Card info={item1} />
-      <Card info={item2} />
-      <Card info={item3} />
-      </>
+      {
+        personagens.map(function (umpersonagem) {
+          return <Card info={umpersonagem} />
+        })
+      }
+    </>
   )
 }
 
